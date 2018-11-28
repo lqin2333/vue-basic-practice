233f5d66ec8a369c19faded11546df09f4b1c086
ryanrunqin <qinrun1989@gmail.com>
2018-11-28 23:07:23 +1300

    Add Vue webpack project

---
+++ vueWeb/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+	<button class="btn btn-primary">wow</button>
+	{{content}}
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App',
+			content: "",
+		}
+	},
+	// after init, invoke this
+	mounted(){
+		this.axios.post("http://api.komavideo.com/news/list").then(body=> {
+			this.content = body.data;
+		});
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
